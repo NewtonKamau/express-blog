a4b7fab5f7ef3779623c4f1ea97920397f968f47
Newton <kamaunewton78@gmail.com>
2020-07-22 22:28:19 +0300

    added about and contact us pages

--- blog/app.js
+++ blog/app.js
@@ -15,6 +15,12 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.render("home", { startingContent: homeContent });
 })
+app.get('/about', (req, res) => {
+   res.render("about", { aboutContent: aboutContent });
+})
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent });
+})
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
