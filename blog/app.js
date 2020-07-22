9c39f9b9457eb4aad3d610996021aba2330d8230
Newton <kamaunewton78@gmail.com>
2020-07-22 22:36:21 +0300

    added compose page and route

--- blog/app.js
+++ blog/app.js
@@ -14,13 +14,16 @@ const app = express();
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.render("home", { startingContent: homeContent });
-})
+});
 app.get('/about', (req, res) => {
-   res.render("about", { aboutContent: aboutContent });
-})
+  res.render("about", { aboutContent: aboutContent });
+});
 app.get("/contact", (req, res) => {
   res.render("contact", { contactContent: contactContent });
-})
+});
+app.get('/compose', (req, res) => {
+  res.render('compose');
+});
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
